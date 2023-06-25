9a1411c7df2d95984503dc1b647243980c9f6d2f
aayamdahal <getaayam@gmail.com>
2023-06-25 09:52:37 +0545

    feat(feat): add users and courses apo

--- apps/express-js-auth-apis/src/index.js
+++ apps/express-js-auth-apis/src/index.js
@@ -6,6 +6,51 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.get("/:id", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.put("/:id", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.delete("/:id", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// users
+app.get("/users", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/users/:id", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.put("/users/:id", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.delete("/users/:id", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// users courses
+app.get("/users/:id/courses", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/users/:id/courses/:id", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.put("/users/:id/courses/:id", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.delete("/users/:id/courses/:id", (req, res) => {
+  res.send("Hello, World!");
+});
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
