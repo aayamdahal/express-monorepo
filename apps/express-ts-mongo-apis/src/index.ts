e09069d7a4e82a501c02000e30765440bef9d158
aayamdahal <getaayam@gmail.com>
2023-07-04 11:50:30 +0545

    feat(src/routes/index.ts): create a basic route

---
+++ apps/express-ts-mongo-apis/src/index.ts
@@ -0,0 +1,26 @@
+require("dotenv").config();
+import express, { Request, Response, NextFunction } from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "../src/router/index";
+
+const app = express();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next();
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on("error", (err: Error) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
+
+app.use("/api", router);
+
+app.listen(process.env.PORT || 8181, () => {
+  console.log(`Server is running on port ${process.env.PORT} || 8181`);
+});
