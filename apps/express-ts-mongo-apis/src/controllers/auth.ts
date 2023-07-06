12eb8722612f21ab6321380ed5f567b44a8e7e24
aayamdahal <getaayam@gmail.com>
2023-07-06 14:09:39 +0545

    feat(auth.js): add login controller

--- apps/express-ts-mongo-apis/src/controllers/auth.ts
+++ apps/express-ts-mongo-apis/src/controllers/auth.ts
@@ -1,1 +1,31 @@
-export const login = async (params: type) => {};
+import { getUserByEmail } from "db/users";
+import express from "express";
+import { Request, Response } from "express";
+import { authentication } from "../helpers";
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    const user = (await getUserByEmail(email).select(
+      "authentication.salt authentication.password"
+    )) as any;
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const expectedHash = authentication(user?.authentication?.salt, password);
+     
+    
+    } catch (error) {
+    console.log(error);
+  }
+};
+export const register = async () => {};
